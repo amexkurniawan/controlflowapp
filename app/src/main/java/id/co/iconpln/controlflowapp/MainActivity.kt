f94b8e8932cac839058e3f88a0aa316be9378fc9
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-19 13:35:56 +0700

    [rahmatkurniawan811] set edit text value, show toast when button clicked

--- app/src/main/java/id/co/iconpln/controlflowapp/MainActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/MainActivity.kt
@@ -2,11 +2,18 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etNilai.setText("100")
+        btnShow.setOnClickListener {
+            Toast.makeText(this, "Click", Toast.LENGTH_LONG).show()
+        }
     }
 }
