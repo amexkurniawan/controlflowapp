5ad606dff5319b2b3d312c9b7ab8fb0f9a55b46b
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-20 12:22:55 +0700

    [rahmatkurniawan811] Classification - set EditText, add button clickListener

--- app/src/main/java/id/co/iconpln/controlflowapp/ClassificationActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/ClassificationActivity.kt
@@ -2,11 +2,20 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_classification.*
 
 class ClassificationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_classification)
+
+        etClassificationNilai.setText("0")
+
+        btnClassificationShow.setOnClickListener {
+
+            
+        }
     }
 }
