16a6f15d18d55c36a20c98dc8095ae71b78419a8
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-20 11:56:38 +0700

    [rahmatkurniawan811] Home - add action to button home activity

--- app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
@@ -1,12 +1,28 @@
 package id.co.iconpln.controlflowapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btnCalculation.setOnClickListener {
+
+            // navigate to activity main
+            val calculationIntent = Intent(this, MainActivity::class.java)
+            startActivity(calculationIntent)
+        }
+
+        btnClassification.setOnClickListener {
+
+            // Toast
+            Toast.makeText(this, "Classification", Toast.LENGTH_LONG).show()
+        }
     }
 }
