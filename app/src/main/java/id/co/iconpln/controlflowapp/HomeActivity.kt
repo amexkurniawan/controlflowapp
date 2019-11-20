8e8a7d22ef6ca4b05cb8591027c89f7b98ee2171
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-20 12:02:39 +0700

    [rahmatkurniawan811] Home - Add onClick listener in home

--- app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
@@ -3,10 +3,11 @@ package id.co.iconpln.controlflowapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +25,9 @@ class HomeActivity : AppCompatActivity() {
             // Toast
             Toast.makeText(this, "Classification", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onClick(view: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
