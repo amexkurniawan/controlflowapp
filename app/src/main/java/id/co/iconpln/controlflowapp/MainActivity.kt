252f11e3465bac3869efee48d2be9a7f134e860d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-20 11:23:05 +0700

    [rahmatkurniawan811] add more function to calculate

--- app/src/main/java/id/co/iconpln/controlflowapp/MainActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/MainActivity.kt
@@ -16,15 +16,21 @@ class MainActivity : AppCompatActivity() {
         // Get the widgets
         val textView = findViewById<TextView>(R.id.tvTextHold)
         val button = findViewById<Button>(R.id.btnShow);
+        
         etNilai.setText("100")
 
         btnShow.setOnClickListener {
-            textView.text = etNilai.text
+            val angka = etNilai.text.toString().toInt()
+            hitungPangkat(angka)
         }
 
         btnShowToast.setOnClickListener {
             Toast.makeText(this, etNilai.text, Toast.LENGTH_LONG).show()
         }
+    }
 
+    fun hitungPangkat(nilai: Int) {
+        val hitungPangkat = nilai * nilai
+        tvTextHold.text = "Hasilnya: $hitungPangkat"
     }
 }
