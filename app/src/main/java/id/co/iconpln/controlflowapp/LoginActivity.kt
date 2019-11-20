342cfb050f1d6d8cdb2003d9147da12b92f00735
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-20 13:33:19 +0700

    [rahmatkurniawan811] Todo:

--- app/src/main/java/id/co/iconpln/controlflowapp/LoginActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/LoginActivity.kt
@@ -8,5 +8,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // TODO: 1. Username & password tidak boleh empty.
+        // TODO: 2. password minimal 7 digit.
+        // TODO: 3. akan sukses jika login: user@mail.com, password
+        // TODO: 4. username mengecek format email
     }
 }
