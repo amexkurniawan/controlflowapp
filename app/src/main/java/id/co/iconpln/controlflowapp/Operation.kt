4db5bf08a745c46b4723150168eaaa0e7ca06f3f
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-27 11:24:42 +0700

    [rahmatkurniawan811] Operation - add operation activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/Operation.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Operation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operation)
+    }
+}
