602f74be4af740379808e4299d835ed57aaf59c6
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-02 12:51:32 +0700

    [rahmatkurniawan811] Demo - override activity

--- app/src/main/java/id/co/iconpln/controlflowapp/DemoActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/DemoActivity.kt
@@ -1,12 +1,50 @@
 package id.co.iconpln.controlflowapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_demo.*
 
 class DemoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
+
+        btnSubmit.setOnClickListener {
+            val styleIntent = Intent(this, StyleActivity::class.java)
+            startActivity(styleIntent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Ame", "-- onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Ame", "-- onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Ame", "-- onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Ame", "-- onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Ame", "-- onRestart")
+    }
+
+    override fun onDestroy() {
+        Log.d("Ame", "-- onDestroy")
+        super.onDestroy()
     }
 }
