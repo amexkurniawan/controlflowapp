d4d793713898210ccf0dcea8727140d4b781b051
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-03 12:03:38 +0700

    [rahmatkurniawan811] Volume - add volume function

--- app/src/main/java/id/co/iconpln/controlflowapp/VolumeActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/VolumeActivity.kt
@@ -2,11 +2,21 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.annotation.IntegerRes
 
 class VolumeActivity : AppCompatActivity() {
+
+    private var volumeResult: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_volume)
+    }
+
+    private fun calculate(length: String, width:String, height: String) {
+        volumeResult = Integer.parseInt(length) * Integer.parseInt(width) * Integer.parseInt(height)
     }
 }
