5935169617708439601fd307926e9a5d1be7f291
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-04 11:54:21 +0700

    [rahmatkurniawan811] Intent - add activities (data, bundle, object, result)

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/IntentWithResultActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntentWithResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_with_result)
+    }
+}
