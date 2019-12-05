5df8739be607408cb65e91717690069e57b63209
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-05 11:50:37 +0700

    [rahmatkurniawan811] Intent - set values for radio button

--- app/src/main/java/id/co/iconpln/controlflowapp/IntentWithResultActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/IntentWithResultActivity.kt
@@ -2,11 +2,29 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_intent_with_result.*
 
-class IntentWithResultActivity : AppCompatActivity() {
+class IntentWithResultActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_with_result)
     }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.btnResultChoose) {
+            if(rgNumber.checkedRadioButtonId != 0) {
+                var value = 0
+                when(rgNumber.checkedRadioButtonId) {
+                    R.id.rb50 -> value = 50
+                    R.id.rb100 -> value = 100
+                    R.id.rb150 -> value = 150
+                    R.id.rb200 -> value = 200
+                }
+            }
+        }
+    }
+        
 }
+
