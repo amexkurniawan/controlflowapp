d25183b437a07f004a618af038b7ef76e35b388f
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-09 12:55:48 +0700

    [rahmatkurniawan] Constraint - add more constraint activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/MoreComplexConstraintActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoreComplexConstraintActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_complex_constraint)
+    }
+}
