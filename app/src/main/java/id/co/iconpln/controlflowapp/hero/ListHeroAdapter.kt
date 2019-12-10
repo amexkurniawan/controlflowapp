930762e04e1cb2eec9bf9f3f9817330e5c94453f
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-10 13:31:18 +0700

    [rahmatkurniawan] ListHero - make viewHolder extend recycler view

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
@@ -1,8 +1,11 @@
 package id.co.iconpln.controlflowapp.hero
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class ListHeroAdapter {
 
-    inner class HeroViewHolder {
+    inner class HeroViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         
     }
 }
