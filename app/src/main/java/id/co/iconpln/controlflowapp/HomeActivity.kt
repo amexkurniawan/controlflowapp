721c6b80474f5a74c3a1b8cf07875689abe7cfa0
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-10 12:55:05 +0700

    [rahmatkurniawan] move activity to directory hero

--- app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/HomeActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import id.co.iconpln.controlflowapp.hero.ListHeroActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity(), View.OnClickListener {
--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroActivity.kt
@@ -1,7 +1,8 @@
-package id.co.iconpln.controlflowapp
+package id.co.iconpln.controlflowapp.hero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
 
 class ListHeroActivity : AppCompatActivity() {
 
