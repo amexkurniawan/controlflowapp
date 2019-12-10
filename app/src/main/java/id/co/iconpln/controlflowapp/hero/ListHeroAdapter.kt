66504edfb9f78583f6a61975c2e166aa827c2051
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-10 13:33:59 +0700

    [rahmatkurniawan] ListHero - extend recycleradapter

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
@@ -3,9 +3,9 @@ package id.co.iconpln.controlflowapp.hero
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ListHeroAdapter {
+class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.HeroViewHolder>() {
 
     inner class HeroViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
-        
+
     }
 }
