3793339f51eadb320d6d150e864c572364c08f9b
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-10 13:34:35 +0700

    [rahmatkurniawan] ListHero - implement function

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
@@ -1,11 +1,24 @@
 package id.co.iconpln.controlflowapp.hero
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.HeroViewHolder>() {
 
     inner class HeroViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
