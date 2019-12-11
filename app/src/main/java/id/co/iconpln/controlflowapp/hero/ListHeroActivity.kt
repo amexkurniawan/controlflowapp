f10d08e245b87a74cb8cd7611a1e7986a09be555
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-11 13:19:24 +0700

    [rahmatkurniawan] GridHero - show grid hero

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.models.Hero
@@ -19,13 +20,15 @@ class ListHeroActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list_hero)
 
         setupListHero()
-        showRecyclerList()
+        //showRecyclerList()
+        showRecyclerGrid()
 
     }
 
     private fun setupListHero() {
         rvListHero.setHasFixedSize(true)
         listHero.addAll(HeroesData.listDataHero)
+        setupListDivider()
     }
 
     private fun setupListDivider() {
@@ -38,12 +41,17 @@ class ListHeroActivity : AppCompatActivity() {
         rvListHero.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(listHero)
         rvListHero.adapter = listHeroAdapter
-
         listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback{
             override fun onItemClick(hero: Hero) {
                 Toast.makeText(this@ListHeroActivity, hero.name, Toast.LENGTH_SHORT).show()
             }
 
         })
+    }
+
+    private fun showRecyclerGrid() {
+        rvListHero.layoutManager = GridLayoutManager(this, 3)
+        val gridHeroAdapter = GridHeroAdapter(listHero)
+        rvListHero.adapter = gridHeroAdapter
     }
 }
