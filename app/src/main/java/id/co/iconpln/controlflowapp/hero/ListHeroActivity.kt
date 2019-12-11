f3f6d21b6139f8990b55b6c82566809d4c088729
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-11 12:02:54 +0700

    [rahmatkurniawan] ListHero - add adapter in listHero activity

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroActivity.kt
@@ -2,12 +2,33 @@ package id.co.iconpln.controlflowapp.hero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.models.Hero
+import id.co.iconpln.controlflowapp.models.HeroesData
+import kotlinx.android.synthetic.main.activity_list_hero.*
 
 class ListHeroActivity : AppCompatActivity() {
+
+    private var listHero: ArrayList<Hero> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_hero)
+
+        setupListHero()
+        showRecyclerList()
+
+    }
+
+    private fun showRecyclerList() {
+        rvListHero.setHasFixedSize(true)
+        listHero.addAll(HeroesData.listDataHero)
+    }
+
+    private fun setupListHero() {
+        rvListHero.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(listHero)
+        rvListHero.adapter = listHeroAdapter
     }
 }
