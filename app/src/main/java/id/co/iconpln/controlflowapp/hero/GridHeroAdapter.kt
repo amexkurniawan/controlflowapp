716a2e3e75b7c557f691999b23b728cfa2448e4a
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-11 12:54:03 +0700

    [rahmatkurniawan] GridHero - make view holder extend recycler view

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
@@ -1,8 +1,11 @@
 package id.co.iconpln.controlflowapp.hero
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class GridHeroAdapter {
 
-    inner class GridHeroHolder{
-        
+    inner class GridHeroHolder(private val view: View): RecyclerView.ViewHolder(view){
+
     }
 }
