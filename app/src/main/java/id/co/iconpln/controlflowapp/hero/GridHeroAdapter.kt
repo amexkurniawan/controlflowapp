5af68db214aa9aac03a8ac73283118e89a703c30
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-11 12:57:21 +0700

    [rahmatkurniawan] GridHero - implement needed function

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/GridHeroAdapter.kt
@@ -1,11 +1,24 @@
 package id.co.iconpln.controlflowapp.hero
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class GridHeroAdapter: RecyclerView.Adapter<GridHeroAdapter.GridHeroHolder>() {
 
     inner class GridHeroHolder(private val view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridHeroHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: GridHeroHolder, position: Int) {
 
     }
 }
