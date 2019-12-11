708c2f542e832ef5c21b06c050bfa24ee5795055
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-11 12:21:58 +0700

    [rahmatkurniawan] ListHero - add item click listener

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroAdapter.kt
@@ -3,6 +3,7 @@ package id.co.iconpln.controlflowapp.hero
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import id.co.iconpln.controlflowapp.R
@@ -22,6 +23,9 @@ class ListHeroAdapter(val listHero: ArrayList<Hero>) : RecyclerView.Adapter<List
 
     override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
         holder.bind(listHero[position])
+        holder.itemView.setOnClickListener {
+            Toast.makeText(holder.itemView.context, listHero[position].name, Toast.LENGTH_SHORT).show()
+        }
     }
 
     inner class HeroViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
