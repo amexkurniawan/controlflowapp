43933b7699c5c2ce0e8e88fb9e3f02f7666e7d3d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-12 13:08:36 +0700

    [rahmatkurniawan] Fragment - override onViewCreate, implement View.OnClickListener

--- app/src/main/java/id/co/iconpln/controlflowapp/fragments/OtherFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/OtherFragment.kt
@@ -6,13 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.fragment_other.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class OtherFragment : Fragment() {
+class OtherFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +24,16 @@ class OtherFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_other, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnLastFragment.setOnClickListener(this)
+    }
 
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.btnLastFragment -> {
+                Toast.makeText(requireContext(), "To Last Content", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
