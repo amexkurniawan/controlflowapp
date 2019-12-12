933e94f9339500ddbdc8ddff3fea1771e7704b87
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-12 11:58:46 +0700

    [rahmatkurniawan] GridHero - add setupGridHero function to add collect data

--- app/src/main/java/id/co/iconpln/controlflowapp/GridHeroActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/GridHeroActivity.kt
@@ -2,11 +2,23 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.co.iconpln.controlflowapp.models.Hero
+import id.co.iconpln.controlflowapp.models.HeroesData
+import kotlinx.android.synthetic.main.activity_list_hero.*
 
 class GridHeroActivity : AppCompatActivity() {
+
+    private var listHero: ArrayList<Hero> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grid_hero)
+
+        setupGridHero()
+    }
+
+    private fun setupGridHero() {
+        rvListHero.setHasFixedSize(true)
+        listHero.addAll(HeroesData.listDataHero)
     }
 }
