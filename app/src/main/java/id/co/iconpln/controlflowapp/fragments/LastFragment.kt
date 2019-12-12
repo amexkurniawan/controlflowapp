b058b0d99156b372329e6bb6ceca04941af7cb3e
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-12 13:12:32 +0700

    [rahmatkurniawan] Fragment - Add Last Fragment

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/LastFragment.kt
@@ -0,0 +1,26 @@
+package id.co.iconpln.controlflowapp.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import id.co.iconpln.controlflowapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LastFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_last, container, false)
+    }
+
+
+}
