a7196116b593db452894f60d6d581832bd177f35
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-12 13:56:16 +0700

    [rahmatkurniawan] Fragment - move to other activity

--- app/src/main/java/id/co/iconpln/controlflowapp/fragments/LastFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/LastFragment.kt
@@ -1,6 +1,7 @@
 package id.co.iconpln.controlflowapp.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.StyleActivity
 import kotlinx.android.synthetic.main.fragment_last.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class LastFragment : Fragment() {
+class LastFragment : Fragment(), View.OnClickListener {
 
     companion object {
         const val EXTRA_NAME_FRAGMENT = "extra name fragment"
@@ -28,12 +30,25 @@ class LastFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_last, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btnOtherActivity.setOnClickListener(this)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         val name: String? = arguments?.getString(EXTRA_NAME_FRAGMENT)
         tvLastName.text = name
         tvLastMsg.text = message
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btnOtherActivity -> {
+                val styleIntent = Intent(requireContext(), StyleActivity::class.java)
+                startActivity(styleIntent)
+            }
+        }
     }
 
 
