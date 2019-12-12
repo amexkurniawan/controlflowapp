f0f883f456f0d98c21e63ed3c10987b70ab34ea6
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-12 12:09:58 +0700

    [rahmatkurniawan] Fragment - add fragment activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/DemoFragmentActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoFragmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_fragment)
+    }
+}
