7eef4a398f5eaf002296ffb98b0e822c0a11233e
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-13 13:12:45 +0700

    [rahmatkurniawan] Fragment - implement View.OnclickListener to OptionDialogFragment

--- app/src/main/java/id/co/iconpln/controlflowapp/fragments/OptionDialogFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragments/OptionDialogFragment.kt
@@ -6,14 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.fragment_last.*
+import kotlinx.android.synthetic.main.fragment_option_dialog.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class OptionDialogFragment : DialogFragment() {
+class OptionDialogFragment : DialogFragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +24,17 @@ class OptionDialogFragment : DialogFragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_option_dialog, container, false)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btnPilih -> {
+                Toast.makeText(requireContext(), "Pilih dongs", Toast.LENGTH_SHORT).show()
+            }
+            R.id.btnClose -> {
+                Toast.makeText((requireContext(), "Close dong", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
