2efba3a3e82860066eb0c26878ae245d8d5464af
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 13:01:16 +0700

    [rahmatkurniawan] Fragment - set navigation item selected listener

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentNav/BottomNavActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNav/BottomNavActivity.kt
@@ -2,12 +2,32 @@ package id.co.iconpln.controlflowapp.fragmentNav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_bottom_nav.*
 
 class BottomNavActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_nav)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        navViewBottom.setOnNavigationItemSelectedListener()
+    }
+
+    private val onNavigationItemSelectedListener: BottomNavigationView.OnNavigationItemSelectedListener
+        = object : BottomNavigationView.OnNavigationItemSelectedListener{
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            when(item.itemId){
+                R.id.navigation_home -> return true
+                R.id.navigation_person -> return true
+            }
+            return false
+        }
     }
 }
