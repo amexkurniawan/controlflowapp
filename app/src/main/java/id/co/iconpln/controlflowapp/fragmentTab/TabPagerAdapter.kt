be899f82c7a766b0a66496df18363a9c5ff149a5
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 12:26:09 +0700

    [rahmatkurniawan] Fragment - add title in tab layout

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabPagerAdapter.kt
@@ -1,11 +1,18 @@
 package id.co.iconpln.controlflowapp.fragmentTab
 
 import android.content.Context
+import androidx.annotation.Nullable
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import id.co.iconpln.controlflowapp.R
 
 class TabPagerAdapter(private val context: Context, fm: FragmentManager): FragmentPagerAdapter(fm) {
+
+    @StringRes
+    private val TAB_TITLES = intArrayOf(R.string.tab_text_1, R.string.tab_text_2)
+
     override fun getItem(position: Int): Fragment {
         var fragment: Fragment? = null
         when(position){
@@ -17,6 +24,11 @@ class TabPagerAdapter(private val context: Context, fm: FragmentManager): Fragme
 
     override fun getCount(): Int {
         return 2
+    }
+
+    @Nullable
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
     }
 
 }
