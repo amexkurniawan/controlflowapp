9bdf94b3581241c621becc82d932ea3aefe61ec2
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 11:30:20 +0700

    Fragment - add tab activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.fragmentTab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class TabActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab)
+    }
+}
