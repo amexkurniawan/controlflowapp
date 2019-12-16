b9d211760a78dba74402e3f0021723249e9da42d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 12:04:23 +0700

    [rahmatkurniawan] Fragment - overide get item function

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabPagerAdapter.kt
@@ -1,9 +1,22 @@
 package id.co.iconpln.controlflowapp.fragmentTab
 
 import android.content.Context
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 class TabPagerAdapter(private val context: Context, fm: FragmentManager): FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when(position){
+            0 -> fragment = FirstFragment()
+            1 -> fragment = SecondFragment()
+        }
+        return fragment as Fragment
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
