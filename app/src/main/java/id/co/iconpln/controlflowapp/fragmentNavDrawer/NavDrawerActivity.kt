3cdb48ff38064ea361afb67ed278a9d2a8263919
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 13:17:43 +0700

    [rahmatkurniawan] Fragment - add nav drawer activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.fragmentNavDrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class NavDrawerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_drawer)
+    }
+}
