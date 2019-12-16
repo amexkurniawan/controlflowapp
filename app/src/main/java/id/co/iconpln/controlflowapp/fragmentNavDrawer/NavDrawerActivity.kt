25767d56d9eadcdabde4ba9bc4f541c5f9e863f8
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 13:58:12 +0700

    [rahmatkurniawan] FragmentDrawer - setup actionbar for nav drawer

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
@@ -2,12 +2,28 @@ package id.co.iconpln.controlflowapp.fragmentNavDrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_nav_drawer.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class NavDrawerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_drawer)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar)
+
+        var toogle = ActionBarDrawerToggle(
+            this, dlDrawerLayout, toolbar, R.string.app_name, 0
+        )
+
+        dlDrawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
     }
 }
