3c963d9a8fcea6a2721d7d0ac8bfb3eead08fc04
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 12:09:42 +0700

    [rahmatkurniawan] Fragment - create function setup viewpager adapter

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabActivity.kt
@@ -3,11 +3,22 @@ package id.co.iconpln.controlflowapp.fragmentTab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_tab.*
 
 class TabActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tab)
+
+        setupTab()
+    }
+
+    private fun setupTab() {
+        val tabPagerAdapter = TabPagerAdapter(this, supportFragmentManager)
+        vpTab.adapter = tabPagerAdapter
+        tab.setupWithViewPager(vpTab)
+
+        supportActionBar?.elevation = 0f
     }
 }
