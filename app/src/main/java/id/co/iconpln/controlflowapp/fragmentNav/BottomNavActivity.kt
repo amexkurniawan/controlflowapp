9bbc870a255da7976d69500cd9a4aeb0bc45412d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 12:31:50 +0700

    [rahmatkurniawan] Fragment - add bottomNavActivity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNav/BottomNavActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp.fragmentNav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomNavActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+    }
+}
