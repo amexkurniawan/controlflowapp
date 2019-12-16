a11470a6fe55deb1fb4524482c5a59b50d7338ac
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-16 11:59:05 +0700

    [rahmatkurniawan] Fragment - add initial pager adapter

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentTab/TabPagerAdapter.kt
@@ -1,5 +1,9 @@
 package id.co.iconpln.controlflowapp.fragmentTab
 
-class TabPagerAdapter {
+import android.content.Context
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class TabPagerAdapter(private val context: Context, fm: FragmentManager): FragmentPagerAdapter(fm) {
 
 }
