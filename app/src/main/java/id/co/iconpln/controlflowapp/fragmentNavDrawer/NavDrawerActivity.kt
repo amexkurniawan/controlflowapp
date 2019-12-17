6ea99f7a1f67b335fab28a96cc7b281dc220ed87
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-17 12:06:30 +0700

    [rahmatkurniawan] NavDrawer - Add Navigation Click in tab drawer

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
@@ -2,18 +2,23 @@ package id.co.iconpln.controlflowapp.fragmentNavDrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_nav_drawer.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
-class NavDrawerActivity : AppCompatActivity() {
+class NavDrawerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_drawer)
 
         setupActionBar()
+        navViewDrawer.setNavigationItemSelectedListener(this)
     }
 
     private fun setupActionBar() {
@@ -25,5 +30,21 @@ class NavDrawerActivity : AppCompatActivity() {
 
         dlDrawerLayout.addDrawerListener(toogle)
         toogle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home_drawer -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_person_drawer -> {
+                Toast.makeText(this, "Person", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_gelery_drawer -> {
+                Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show()
+            }
+        }
+        dlDrawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
