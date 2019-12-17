3d7eb330155123e5d587203f93746ea77e2f61a9
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-17 11:53:17 +0700

    [rahmatkurniawan] HeroFragment - call recyclerview adapter in listHeroFragment

--- app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/hero/ListHeroFragment.kt
@@ -1,18 +1,27 @@
 package id.co.iconpln.controlflowapp.hero
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.models.Hero
+import id.co.iconpln.controlflowapp.models.HeroesData
+import kotlinx.android.synthetic.main.fragment_list_hero.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ListHeroFragment : Fragment() {
+
+    private var listHero: ArrayList<Hero> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +31,33 @@ class ListHeroFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list_hero, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListHero()
+        showRecyclerViewList(view.context)
+    }
 
+    private fun setupListHero() {
+        rvFragmentListHero.setHasFixedSize(true)
+        listHero.addAll(HeroesData.listDataHero)
+        setupListDivider()
+    }
+
+    private fun setupListDivider() {
+        val dividerItemDecoration = DividerItemDecoration(
+            rvFragmentListHero.context, DividerItemDecoration.VERTICAL )
+        rvFragmentListHero.addItemDecoration(dividerItemDecoration)
+    }
+
+    private fun showRecyclerViewList(context: Context) {
+        rvFragmentListHero.layoutManager = LinearLayoutManager(context)
+        val listHeroAdapter = ListHeroAdapter(listHero)
+        rvFragmentListHero.adapter = listHeroAdapter
+        listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback{
+            override fun onItemClick(hero: Hero) {
+                Toast.makeText(requireContext(), hero.name, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
 }
