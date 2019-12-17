7f6526496242cc32faeb4c92224e9ed3670d000d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-17 12:19:45 +0700

    [rahmatkurniawan] NavDrawer - Open Activity when navigation item selected

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentNavDrawer/NavDrawerActivity.kt
@@ -1,5 +1,6 @@
 package id.co.iconpln.controlflowapp.fragmentNavDrawer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -9,6 +10,7 @@ import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.StyleActivity
 import id.co.iconpln.controlflowapp.fragmentTab.FirstFragment
 import id.co.iconpln.controlflowapp.fragmentTab.SecondFragment
 import kotlinx.android.synthetic.main.activity_nav_drawer.*
@@ -46,7 +48,8 @@ class NavDrawerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
                 Toast.makeText(this, "Person", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_gelery_drawer -> {
-                loadFragment(SecondFragment())
+                val openActivityIntent = Intent(this, StyleActivity::class.java)
+                startActivity(openActivityIntent)
                 Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show()
             }
         }
