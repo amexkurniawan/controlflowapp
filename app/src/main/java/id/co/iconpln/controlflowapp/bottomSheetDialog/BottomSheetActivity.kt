bf7dc356a6e118d4390f1726241fd1baa4489540
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-17 20:51:18 -0800

    [rahmatkurniawan] BottomSheetDialog - add BottomSheet activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/bottomSheetDialog/BottomSheetActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.bottomSheetDialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class BottomSheetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_sheet)
+    }
+}
