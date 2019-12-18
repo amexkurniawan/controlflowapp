933f46c0fd120e7c82b1d64202faff1f9b91631a
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-17 20:57:25 -0800

    [rahmatkurniawan] BottomSheetDialog - add BottomSheet activity and onclicklistener

--- app/src/main/java/id/co/iconpln/controlflowapp/bottomSheetDialog/BottomSheetActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/bottomSheetDialog/BottomSheetActivity.kt
@@ -2,12 +2,37 @@ package id.co.iconpln.controlflowapp.bottomSheetDialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_bottom_sheet.*
 
-class BottomSheetActivity : AppCompatActivity() {
+class BottomSheetActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_sheet)
+
+        setOnClickButton()
     }
+
+    private fun setOnClickButton() {
+        btnBottomSheet.setOnClickListener(this)
+        btnBottomSheetDialog.setOnClickListener(this)
+        btnBottomSheetDialogFragment.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnBottomSheet -> {
+
+            }
+            R.id.btnBottomSheetDialog -> {
+
+            }
+            R.id.btnBottomSheetDialogFragment -> {
+
+            }
+        }
+    }
+
 }
