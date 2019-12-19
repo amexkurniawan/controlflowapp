5ae3650d1957c646b4d8d1ef4d5f89d5cbda10af
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-19 12:03:47 +0700

    [rahmatkurniawan] Bottom Sheet Fragment - add click listener to bottom sheet fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/bottomSheetDialog/BottomSheetFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/bottomSheetDialog/BottomSheetFragment.kt
@@ -9,11 +9,12 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.fragment_bottom_sheet.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class BottomSheetFragment : BottomSheetDialogFragment() {
+class BottomSheetFragment : BottomSheetDialogFragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +23,41 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener(){
+        llBottomEdit.setOnClickListener(this)
+        llBottomShare.setOnClickListener(this)
+        llBottomEdit.setOnClickListener(this)
+        llBottomSearch.setOnClickListener(this)
+        llBottomExit.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            llBottomEdit.id -> {
+
+            }
+            llBottomShare.id -> {
+
+            }
+            llBottomEdit.id -> {
+
+            }
+            llBottomSearch.id -> {
+
+            }
+            llBottomExit.id -> {
+
+            }
+        }
+    }
+
 
 
 }
