e270e560ee0d4879c082c84e19e8c3c72ab2acd8
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-20 11:40:24 +0700

    [rahmatkurniawan] Fragment View Pager - add activity, fragment, adapter and implement in gradle

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp.fragmentViewPager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll)
+    }
+}
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollPagerAdapter.kt
@@ -0,0 +1,5 @@
+package id.co.iconpln.controlflowapp.fragmentViewPager
+
+class ScrollPagerAdapter {
+
+}
