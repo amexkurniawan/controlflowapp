0718b718c8713167ad3fe0b9e3999ac53eaf2cca
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-20 12:39:33 +0700

    [rahmatkurniawan] Localization - override onCreateOptionMenu function

--- app/src/main/java/id/co/iconpln/controlflowapp/LocalizationActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/LocalizationActivity.kt
@@ -2,11 +2,18 @@ package id.co.iconpln.controlflowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class LocalizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_localization)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_localization, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
