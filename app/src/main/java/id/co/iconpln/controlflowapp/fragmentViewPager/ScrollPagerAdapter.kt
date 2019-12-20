6fde35f3315edc2eac83645fe30fbeb175236232
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-20 13:18:55 +0700

    [rahmatkurniawan] Scroll View Pager -  Setup item in adapter

--- app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollPagerAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/fragmentViewPager/ScrollPagerAdapter.kt
@@ -1,5 +1,29 @@
 package id.co.iconpln.controlflowapp.fragmentViewPager
 
-class ScrollPagerAdapter {
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import id.co.iconpln.controlflowapp.fragmentTab.FirstFragment
+import id.co.iconpln.controlflowapp.fragmentTab.SecondFragment
 
+class ScrollPagerAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+
+    companion object{
+        private const val NUM_PAGES = 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when(position){
+            0 -> fragment = FirstFragment()
+            1 -> fragment = SecondFragment()
+            2 -> fragment = ScrollFragment()
+        }
+        return fragment as Fragment
+    }
+
+    override fun getCount(): Int {
+        return NUM_PAGES
+    }
 }
