9e1d9311cd51ceb217435844ae4bfbb3a237b970
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-20 13:02:06 +0700

    [rahmatkurniawan] Localization - Add plural string resources

--- app/src/main/java/id/co/iconpln/controlflowapp/LocalizationActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/LocalizationActivity.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_localization.*
 
 class LocalizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_localization)
+
+        displayLocalizedTexts()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -26,5 +29,20 @@ class LocalizationActivity : AppCompatActivity() {
             startActivity(settingsIntent)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun displayLocalizedTexts(){
+        val pokeCount = 3
+        val helo: String = resources.getString(
+            R.string.locale_count_hello, "Iconplus", pokeCount, "ame"
+        )
+        tvLocalHello.text = helo
+
+        // plural
+        val songCount = 5
+        val pluralText = resources.getQuantityText(
+            R.plurals.locale_count_plural_songs, songCount
+        )
+        tvLocalPlural.text = pluralText
     }
 }
