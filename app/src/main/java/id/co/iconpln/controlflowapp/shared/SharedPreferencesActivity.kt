f67d05c3b97fe188b9681b0206f59f6881fd0ca2
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-06 11:37:02 +0700

    [rahmatkurniawan] SharedPreferences - add activities

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/shared/SharedPreferencesActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp.shared
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharedPreferencesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preferences)
+    }
+}
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/shared/SharedPreferencesFormActivity.kt
@@ -0,0 +1,12 @@
+package id.co.iconpln.controlflowapp.shared
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharedPreferencesFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preferences_form)
+    }
+}
