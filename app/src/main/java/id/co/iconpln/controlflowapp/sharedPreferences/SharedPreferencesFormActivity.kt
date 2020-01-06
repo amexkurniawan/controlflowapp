768d05d60fd5c8c70b0b3c80a03a1ce259decea7
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-06 13:08:58 +0700

    [rahmatkurniawan] SharedPreferences - add companion object

--- app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesFormActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesFormActivity.kt
@@ -9,6 +9,14 @@ import kotlinx.android.synthetic.main.activity_shared_preferences_form.*
 
 class SharedPreferencesFormActivity : AppCompatActivity(), View.OnClickListener {
 
+    companion object{
+        const val EXTRA_TYPE_FORM = "extra_type_form"
+        const val EXTRA_RESULT = "extra_result"
+        const val RESULT_CODE = 101
+        const val TYPE_ADD = 1
+        const val TYPE_EDIT = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preferences_form)
