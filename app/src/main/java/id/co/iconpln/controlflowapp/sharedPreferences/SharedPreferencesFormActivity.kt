dcb9d829e003c9e9a84414bff9c923041dce60e4
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-06 13:06:07 +0700

    [rahmatkurniawan] SharedPreferences - set onClickListener in  shared preferences form activity

--- app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesFormActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesFormActivity.kt
@@ -2,12 +2,23 @@ package id.co.iconpln.controlflowapp.sharedPreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_shared_preferences_form.*
 
-class SharedPreferencesFormActivity : AppCompatActivity() {
+class SharedPreferencesFormActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preferences_form)
+
+        btnPrefFormSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btnPrefFormSave){
+            Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
