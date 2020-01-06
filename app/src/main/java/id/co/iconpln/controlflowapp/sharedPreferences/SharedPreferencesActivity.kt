676e9d36a6ebec0eea6bbd93ff0fc547a5ab8226
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-06 12:49:18 +0700

    [rahmatkurniawan] SharedPreferences - set click listener intent

--- app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesActivity.kt
@@ -1,13 +1,25 @@
 package id.co.iconpln.controlflowapp.sharedPreferences
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_shared_preferences.*
 
-class SharedPreferencesActivity : AppCompatActivity() {
+class SharedPreferencesActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preferences)
+
+        btnPrefSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.btnPrefSave){
+            val sharedPrefFormIntent = Intent(this, SharedPreferencesFormActivity::class.java)
+            startActivity(sharedPrefFormIntent)
+        }
     }
 }
