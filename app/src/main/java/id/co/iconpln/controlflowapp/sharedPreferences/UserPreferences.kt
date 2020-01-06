bd23bf060c6b4d57c2f2f1bbcd461e80cdcdb5c9
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-06 12:33:23 +0700

    [rahmatkurniawan] SharedPreferences - add user preference class

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/UserPreferences.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.sharedPreferences
+
+internal class UserPreferences{
+
+    companion object{
+        private const val PREFS_USER = "user_prefs"
+        private const val NAME = "name"
+        private const val EMAIL = "email"
+        private const val AGE = "age"
+        private const val HANDPHONE = "handphone"
+        private const val HAS_READING_HOBBY = "hasReadingHobby"
+    }
+}
