aa2fc338d67385d3fdb9dff451745ef45c6fe12c
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-07 12:13:53 +0700

    [rahmatkurniawan] SharedPreferences - populate user data from SP

--- app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/sharedPreferences/SharedPreferencesActivity.kt
@@ -25,6 +25,20 @@ class SharedPreferencesActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun showExistingPreference(){
         user = userPreferences.getUser()
+        populateView(user)
+    }
+
+    private fun populateView(user: User) {
+        tvPrefName.text = if (user.name.toString().isEmpty())
+            "Tidak Ada" else user.name
+        tvPrefAge.text = if (user.age.toString().isEmpty())
+            "Tidak Ada" else user.age.toString()
+        tvPrefEmail.text = if (user.email.toString().isEmpty())
+            "Tidak Ada" else user.email
+        tvPrefHandphone.text = if (user.handphone.toString().isEmpty())
+            "Tidak Ada" else user.handphone
+        tvPrefHobby.text = if (!user.hasReadingHobby)
+            "Tidak Membaca" else "Membaca"
     }
 
     override fun onClick(view: View) {
