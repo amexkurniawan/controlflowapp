66bfcb35ec434264e1de468d0a3410490ae4cbde
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-07 13:24:17 +0700

    [rahmatkurniawan] Weather - Show List Weather in activity

--- app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherActivity.kt
@@ -2,12 +2,26 @@ package id.co.iconpln.controlflowapp.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_weather.*
 
 class WeatherActivity : AppCompatActivity() {
+
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        showListWeather()
+    }
+
+    private fun showListWeather() {
+        adapter = WeatherAdapter()
+        adapter.notifyDataSetChanged()
+
+        rvWeatherList.layoutManager = LinearLayoutManager(this)
+        rvWeatherList.adapter = adapter
     }
 }
