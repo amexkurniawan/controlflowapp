7ec2846bbbd7e5ad7bfee03ac14711e38b4e21b6
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-07 13:18:27 +0700

    [rahmatkurniawan] Weather - implement needed function

--- app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherAdapter.kt
@@ -1,23 +1,35 @@
 package id.co.iconpln.controlflowapp.weather
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.item_list_weather.view.*
 
 class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+
+    private val weatherData = ArrayList<Weather>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list_weather, parent, false)
+        return WeatherViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return weatherData.size
     }
 
     override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(weatherData[position])
     }
 
     inner class WeatherViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
+        fun bind(weatherItem: Weather) {
+            itemView.tvWeatherCity.text = weatherItem.name
+            itemView.tvWeatherTemp.text = weatherItem.temperature
+            itemView.tvWeatherDesc.text = weatherItem.description
+        }
     }
 }
