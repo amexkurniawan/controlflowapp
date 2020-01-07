9d70cd62c01014c8d95a54392a4a3ccdbd0deb66
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-07 13:33:48 +0700

    [rahmatkurniawan] Weather - Add function to set and get weather

--- app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherViewModel.kt
@@ -1,7 +1,18 @@
 package id.co.iconpln.controlflowapp.weather
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class WeatherViewModel : ViewModel(){
-    
+
+    private val listWeathers = MutableLiveData<ArrayList<Weather>>()
+
+    internal fun setWeather(city: String) {
+
+    }
+
+    internal fun getWeather(): LiveData<ArrayList<Weather>>{
+        return listWeathers
+    }
 }
