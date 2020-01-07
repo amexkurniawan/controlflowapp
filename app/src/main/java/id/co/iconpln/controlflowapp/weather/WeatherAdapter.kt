59cc8742223c3f0fb89941c782a1bb864b049c21
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-07 13:11:55 +0700

    [rahmatkurniawan] Weather - Override function for recyclerview

--- app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherAdapter.kt
@@ -1,11 +1,23 @@
 package id.co.iconpln.controlflowapp.weather
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class WeatherAdapter {
+class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     inner class WeatherViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        
+
     }
 }
