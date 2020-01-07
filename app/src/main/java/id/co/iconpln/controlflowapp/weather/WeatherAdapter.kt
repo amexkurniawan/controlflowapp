593f5db04f35dbe64b99403ae9322cc90a4c5a25
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-07 13:09:17 +0700

    [rahmatkurniawan] Weather - add initial adapter

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherAdapter.kt
@@ -0,0 +1,11 @@
+package id.co.iconpln.controlflowapp.weather
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class WeatherAdapter {
+
+    inner class WeatherViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        
+    }
+}
