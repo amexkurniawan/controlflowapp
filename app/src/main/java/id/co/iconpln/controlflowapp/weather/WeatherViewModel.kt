41789f7e5989b89427006243d8372bd4bf9f0ca3
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-07 13:40:14 +0700

    [rahmatkurniawan] Weather - configure build config for api

--- app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherViewModel.kt
@@ -3,8 +3,13 @@ package id.co.iconpln.controlflowapp.weather
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.co.iconpln.controlflowapp.BuildConfig
 
 class WeatherViewModel : ViewModel(){
+
+    companion object{
+        private const val API_KEY = BuildConfig.API_KEY
+    }
 
     private val listWeathers = MutableLiveData<ArrayList<Weather>>()
 
