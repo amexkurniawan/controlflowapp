26d8c704b05a1a078a46a5b2a93805afa6201d5d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 13:24:35 +0700

    [rahmatkurniawan] Contact - Request Contact Api

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactViewModel.kt
@@ -3,12 +3,20 @@ package id.co.iconpln.controlflowapp.contact
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
 
 class ContactViewModel : ViewModel() {
     private val listContact = MutableLiveData<ArrayList<Contact>>()
 
     internal fun setContact(){
+        val client = AsyncHttpClient()
+        val listContacts = ArrayList<Contact>()
+        val url = "https://api.androidhive.info/contacts/"
 
+        client.get(url, object : AsyncHttpResponseHandler() {
+            
+        })
     }
 
     internal fun getContact(): LiveData<ArrayList<Contact>>{
