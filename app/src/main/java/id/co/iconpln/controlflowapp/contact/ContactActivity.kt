1080858144965d174c509db0cf2529b200b75f6d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 13:18:22 +0700

    [rahmatkurniawan] Contact - initial view model

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactActivity.kt
@@ -2,6 +2,8 @@ package id.co.iconpln.controlflowapp.contact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_contact.*
@@ -9,12 +11,18 @@ import kotlinx.android.synthetic.main.activity_contact.*
 class ContactActivity : AppCompatActivity() {
 
     private lateinit var adapter: ContactAdapter
+    private lateinit var contactViewModel: ContactViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
+        initialViewModel()
         showListContact()
+    }
+
+    private fun initialViewModel() {
+        contactViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(ContactViewModel::class.java)
     }
 
     private fun showListContact() {
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactViewModel.kt
@@ -0,0 +1,7 @@
+package id.co.iconpln.controlflowapp.contact
+
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel() {
+
+}
