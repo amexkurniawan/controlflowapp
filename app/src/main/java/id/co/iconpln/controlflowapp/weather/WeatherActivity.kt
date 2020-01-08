880cbe83aceac63d3a7a6afbf1739c3d419a5898
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 12:07:31 +0700

    [rahmatkurniawan] Contact - Init View Model

--- app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/weather/WeatherActivity.kt
@@ -2,6 +2,7 @@ package id.co.iconpln.controlflowapp.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_weather.*
@@ -9,12 +10,20 @@ import kotlinx.android.synthetic.main.activity_weather.*
 class WeatherActivity : AppCompatActivity() {
 
     private lateinit var adapter: WeatherAdapter
+    private lateinit var weatherViewModel: WeatherViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
 
+        initViewModel()
         showListWeather()
+    }
+
+    
+    private fun initViewModel(){
+        weatherViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(WeatherViewModel::class.java)
     }
 
     private fun showListWeather() {
