8e93eb4f3e0b251280efe16e2fc8e6b35de72622
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 13:22:14 +0700

    [rahmatkurniawan] Contact - add function in view model to set and get contact

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactViewModel.kt
@@ -1,7 +1,17 @@
 package id.co.iconpln.controlflowapp.contact
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ContactViewModel : ViewModel() {
+    private val listContact = MutableLiveData<ArrayList<Contact>>()
 
+    internal fun setContact(){
+
+    }
+
+    internal fun getContact(): LiveData<ArrayList<Contact>>{
+        return listContact
+    }
 }
