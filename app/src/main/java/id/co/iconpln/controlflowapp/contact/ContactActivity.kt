2b99740cfd2f3baa2b35196029abf02e202baeac
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 12:04:08 +0700

    [rahmatkurniawan] Contact - add function to call adapter in contact activity

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactActivity.kt
@@ -2,12 +2,26 @@ package id.co.iconpln.controlflowapp.contact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_contact.*
 
 class ContactActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ContactAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        showListWeather()
+    }
+
+    private fun showListWeather() {
+        adapter = ContactAdapter()
+        adapter.notifyDataSetChanged()
+
+        rvContactList.layoutManager = LinearLayoutManager(this)
+        rvContactList.adapter = adapter
     }
 }
