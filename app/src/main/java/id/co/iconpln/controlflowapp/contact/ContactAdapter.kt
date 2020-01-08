c1e10facdfe90b5c7aab6f45a61ff8c09926d8ac
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 11:57:27 +0700

    [rahmatkurniawan] Contact - add initial contact adapter

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactAdapter.kt
@@ -0,0 +1,11 @@
+package id.co.iconpln.controlflowapp.contact
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+
+    inner class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+}
