cf876b228f7ea91eba123de4229d70cea82f3c3e
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 11:46:54 +0700

    [rahmatkurniawan] Contact - add contact activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.contact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class ContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
