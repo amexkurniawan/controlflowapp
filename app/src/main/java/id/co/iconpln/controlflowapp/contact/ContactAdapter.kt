369249704c4cb626fa10b34e77ff1ed456150680
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-08 12:00:59 +0700

    [rahmatkurniawan] Contact - implement needed function

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactAdapter.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/ContactAdapter.kt
@@ -1,11 +1,33 @@
 package id.co.iconpln.controlflowapp.contact
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.item_list_contact.view.*
 
 class ContactAdapter: RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+    private val contactData = ArrayList<Contact>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list_contact, parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return contactData.size
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        holder.bind(contactData[position])
+    }
 
     inner class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+        fun bind(contactItem: Contact) {
+            itemView.tvContactName.text = contactItem.name
+            itemView.tvContactEmail.text = contactItem.email
+            itemView.tvContactMobile.text = contactItem.mobile
+        }
     }
 }
