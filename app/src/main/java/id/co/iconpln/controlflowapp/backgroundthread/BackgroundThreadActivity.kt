fe4cddda05904dace6697cb6f6e9bc3b63813b78
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-09 12:38:59 +0700

    [rahmatkurniawan] Background Thread - Don't call network in main thread

--- app/src/main/java/id/co/iconpln/controlflowapp/backgroundthread/BackgroundThreadActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/backgroundthread/BackgroundThreadActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import id.co.iconpln.controlflowapp.R
 import kotlinx.android.synthetic.main.activity_background_thread.*
+import java.net.URL
 
 class BackgroundThreadActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -24,7 +25,12 @@ class BackgroundThreadActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         when(view.id){
-            R.id.btnThreadWorker -> {}
+            R.id.btnThreadWorker -> {
+                /* // Can't call Network in main Thread
+                val text = URL("https://api.androidhive.info/contacts/").readText()
+                tvThreadWorkerResult.text = text
+                */
+            }
             R.id.btnThreadHandler -> {}
             R.id.btnThreadAsyncTask -> {}
             R.id.btnThreadCoroutine -> {}
