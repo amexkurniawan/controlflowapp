f2e9fa72e4d079c22ed320669970d1f96f77da36
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-09 11:32:58 +0700

    [rahmatkurniawan] Background Thread - add background thread activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/backgroundthread/BackgroundThreadActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.backgroundthread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class BackgroundThreadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_background_thread)
+    }
+}
