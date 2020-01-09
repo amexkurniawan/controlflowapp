1be35b66afb51652ca2e767fd7c5df676532fde6
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-09 11:47:39 +0700

    [rahmatkurniawan] Contact Fragment - add contact tab activity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.contactFragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class ContactTabActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_tab)
+    }
+}
