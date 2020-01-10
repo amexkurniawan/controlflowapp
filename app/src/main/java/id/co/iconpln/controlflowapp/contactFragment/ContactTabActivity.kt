a980deba524f10562e9573f58379f920534e712e
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-10 11:44:33 +0700

    [rahmatkurniawan] Contact Fragment - setting fragment in contact tab activity

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactTabActivity.kt
@@ -9,5 +9,16 @@ class ContactTabActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_tab)
+
+        initFragment()
+    }
+
+    private fun initFragment() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTrasaction = fragmentManager.beginTransaction()
+
+        val fragment = ContactFragment()
+        fragmentTrasaction.add(R.id.flContainer, fragment)
+        fragmentTrasaction.commit()
     }
 }
