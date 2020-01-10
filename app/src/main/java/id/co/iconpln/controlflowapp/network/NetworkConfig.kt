66ed87f83410e1ddd097d7726b84ca8b78b18020
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-10 13:54:58 +0700

    [rahmatkurniawan] Retrofit - add logging interceptor

--- app/src/main/java/id/co/iconpln/controlflowapp/network/NetworkConfig.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/network/NetworkConfig.kt
@@ -1,8 +1,11 @@
 package id.co.iconpln.controlflowapp.network
 
 import id.co.iconpln.controlflowapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class NetworkConfig {
 
@@ -24,6 +27,18 @@ class NetworkConfig {
             return Retrofit.Builder()
                 .baseUrl(BuildConfig.CONTACT_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        private fun getInterceptor(): OkHttpClient {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            return OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
                 .build()
         }
     }
