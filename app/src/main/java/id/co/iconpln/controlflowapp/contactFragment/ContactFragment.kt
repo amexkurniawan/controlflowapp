0f83714ae2d3313095d18dc5361bfbe7037dfb53
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-10 11:32:29 +0700

    [rahmatkurniawan] Contact Fragment - setting view model in contact fragment

--- app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactFragment.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contactFragment/ContactFragment.kt
@@ -6,13 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.contact.ContactViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class ContactFragment : Fragment() {
+
+    private lateinit var contactViewModel: ContactViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,16 @@ class ContactFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initialViewModel()
+    }
+
+    private fun initialViewModel() {
+        contactViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(ContactViewModel::class.java)
     }
 
 
