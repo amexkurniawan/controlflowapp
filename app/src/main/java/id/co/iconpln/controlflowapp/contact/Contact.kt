06f42d4ac3fac81d3ee7b6ab5e879def1cc55020
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-13 12:17:21 +0700

    [rahmatkurniawan] Model - change to data class

--- app/src/main/java/id/co/iconpln/controlflowapp/contact/Contact.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/contact/Contact.kt
@@ -1,8 +1,8 @@
 package id.co.iconpln.controlflowapp.contact
 
-class Contact {
-    var id: String? = "0"
-    var name: String? = null
-    var email: String? = null
+data class Contact (
+    var id: String? = "0",
+    var name: String? = null,
+    var email: String? = null,
     var mobile: String? = null
-}
+)
