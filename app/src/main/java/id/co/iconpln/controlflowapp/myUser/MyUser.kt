fcad798d76182617ef67777905e549415d9f3f17
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-13 11:29:40 +0700

    [rahmatkurniawan] MyUser - add user model

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUser.kt
@@ -0,0 +1,8 @@
+package id.co.iconpln.controlflowapp.myUser
+
+class MyUser {
+    var id: String? = "0"
+    var name: String? = null
+    var address: String? = null
+    var mobile: String? = null
+}
