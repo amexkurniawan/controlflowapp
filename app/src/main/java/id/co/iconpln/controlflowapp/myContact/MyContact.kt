d38541427503e331d4b2260f589692ca36d35511
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-13 10:30:08 +0700

    [rahmatkurniawan] MyContact - add model mycontact

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/myContact/MyContact.kt
@@ -0,0 +1,8 @@
+package id.co.iconpln.controlflowapp.myContact
+
+class MyContact {
+    var id: String? = "0"
+    var name: String? = null
+    var email: String? = null
+    var mobile: String? = null
+}
