5969745927131554bcbf7e4620513e1fd702ab4d
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-13 14:06:47 +0700

    [rahmatkurniawan] My User - add user network repository

--- app/src/main/java/id/co/iconpln/controlflowapp/network/MyUserNetworkRepository.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/network/MyUserNetworkRepository.kt
@@ -3,7 +3,9 @@ package id.co.iconpln.controlflowapp.network
 import androidx.lifecycle.MutableLiveData
 import id.co.iconpln.controlflowapp.models.myUser.BaseUserResponse
 import id.co.iconpln.controlflowapp.models.myUser.UserDataResponse
+import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.Response
 
 class MyUserNetworkRepository {// gate for conect between view model and network
 
@@ -13,7 +15,28 @@ class MyUserNetworkRepository {// gate for conect between view model and network
 
         NetworkConfig.userAPI().getAllUsers().enqueue(object :
         Callback<BaseUserResponse<UserDataResponse>> {
-            
+            override fun onFailure(call: Call<BaseUserResponse<UserDataResponse>>, t: Throwable) {
+                userData.postValue(null)
+            }
+
+            override fun onResponse(
+                call: Call<BaseUserResponse<UserDataResponse>>,
+                response: Response<BaseUserResponse<UserDataResponse>>
+            ) {
+
+                if(response.isSuccessful) {
+                    val listUserSize = response.body()?.data?.size as Int
+
+                    for(i in 0 until listUserSize) {
+                        listUser.add(response.body()?.data?.get(i) as UserDataResponse)
+                    }
+
+                    userData.postValue(listUser)
+                }
+            }
+
         })
+
+        return userData
     }
 }
