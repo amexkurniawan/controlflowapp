e6c1ee8093632c6c6710d9ae8a51184813a2aec2
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-14 12:33:47 +0700

    [rahmatkurniawan] My User - using parcelable in UserDataResponse model

--- app/src/main/java/id/co/iconpln/controlflowapp/models/myUser/UserDataResponse.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/models/myUser/UserDataResponse.kt
@@ -1,8 +1,12 @@
 package id.co.iconpln.controlflowapp.models.myUser
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class UserDataResponse(
     val address: String,
     val id: Int,
     val name: String,
     val phone: String
-)
+): Parcelable
