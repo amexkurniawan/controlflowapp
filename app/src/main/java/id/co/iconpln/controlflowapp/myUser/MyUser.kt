ee74cc4407984d57fb2ae6131c148d92756de181
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-14 12:24:42 +0700

    [rahmatkurniawan] My User - using parceable in MyUser Model

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUser.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUser.kt
@@ -1,8 +1,12 @@
 package id.co.iconpln.controlflowapp.myUser
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class MyUser (
     var id: String? = "0",
     var name: String? = null,
     var address: String? = null,
     var mobile: String? = null
-)
+): Parcelable
