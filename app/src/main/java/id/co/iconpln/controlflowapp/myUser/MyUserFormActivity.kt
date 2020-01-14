6f22c9694de328267efbd436b4316a29324caa53
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-14 12:23:48 +0700

    [rahmatkurniawan] My User - add MyUserForm Activity and styling layout

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.myUser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class MyUserFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_user_form)
+    }
+}
