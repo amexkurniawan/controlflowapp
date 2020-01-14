ecfa803c71baba54bddda367e7db700e7a0d75f3
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-14 13:31:44 +0700

    [rahmatkurniawan] My User - Add function to update user

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormViewModel.kt
@@ -1,7 +1,16 @@
 package id.co.iconpln.controlflowapp.myUser
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.co.iconpln.controlflowapp.models.myUser.UserDataResponse
+import id.co.iconpln.controlflowapp.network.MyUserNetworkRepository
 
 class MyUserFormViewModel : ViewModel(){
 
+    fun updateUser(id: Int, userData: UserDataResponse)
+            : MutableLiveData<UserDataResponse> {
+
+        return MyUserNetworkRepository()
+            .updateUser(id, userData)
+    }
 }
