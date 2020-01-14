b4e02306c385c346d59dca9445a52a75270cbbac
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-14 13:51:10 +0700

    [rahmatkurniawan] My User - add model for delete

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/models/myUser/DeletedUser.kt
@@ -0,0 +1,8 @@
+package id.co.iconpln.controlflowapp.models.myUser
+
+data class DeletedUser(
+    val address: String,
+    val id: Int,
+    val name: String,
+    val phone: String
+)
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/models/myUser/UpdateUserResponse.kt
@@ -0,0 +1,6 @@
+package id.co.iconpln.controlflowapp.models.myUser
+
+data class UpdateUserResponse(
+    val deleted_user: DeletedUser,
+    val message: String
+)
