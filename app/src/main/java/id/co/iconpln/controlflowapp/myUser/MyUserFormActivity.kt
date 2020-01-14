bfa41bcf5160fd454ee331cac6dfcd56bf44f405
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-14 13:30:09 +0700

    [rahmatkurniawan] My User - Initialize view model

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
@@ -2,6 +2,7 @@ package id.co.iconpln.controlflowapp.myUser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.models.myUser.UserDataResponse
 import kotlinx.android.synthetic.main.activity_my_user_form.*
@@ -12,17 +13,23 @@ class MyUserFormActivity : AppCompatActivity() {
         const val EXTRA_USER = "EXTRA_USER"
     }
 
+    private lateinit var viewModel: MyUserFormViewModel
     private lateinit var user: UserDataResponse
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_user_form)
         initIntentExtra()
+        initViewModel()
         populateFormData(user)
     }
 
     private fun initIntentExtra() {
         user = intent.getParcelableExtra(EXTRA_USER) as UserDataResponse
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MyUserFormViewModel::class.java)
     }
 
     private fun populateFormData(user: UserDataResponse){
