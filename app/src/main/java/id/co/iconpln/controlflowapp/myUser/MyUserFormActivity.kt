bca43303665ee9be21c1bd19f9e662f086d4fc68
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-14 13:38:46 +0700

    [rahmatkurniawan] My User - Prepare Update user data to be sent

--- app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myUser/MyUserFormActivity.kt
@@ -2,12 +2,13 @@ package id.co.iconpln.controlflowapp.myUser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import id.co.iconpln.controlflowapp.R
 import id.co.iconpln.controlflowapp.models.myUser.UserDataResponse
 import kotlinx.android.synthetic.main.activity_my_user_form.*
 
-class MyUserFormActivity : AppCompatActivity() {
+class MyUserFormActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val EXTRA_USER = "EXTRA_USER"
@@ -15,6 +16,7 @@ class MyUserFormActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MyUserFormViewModel
     private lateinit var user: UserDataResponse
+    private var userId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,4 +39,27 @@ class MyUserFormActivity : AppCompatActivity() {
         etUserFormAddress.setText(user.address)
         etUserFormHp.setText(user.phone)
     }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.btnUserFormSave -> {
+                userId?.let { id ->
+                    val updateUserData = UserDataResponse(
+                        etUserFormAddress.text.toString(),
+                        id,
+                        etUserFormName.text.toString(),
+                        etUserFormHp.text.toString()
+                    )
+
+                    updateUser(id, updateUserData)
+                }
+            }
+        }
+    }
+
+    private fun updateUser(id: Int, userData: UserDataResponse) {
+
+    }
+
+
 }
