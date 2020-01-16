5d9f851b56d479ada38cee012bc79e4189e51ae8
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-16 13:24:18 +0700

    [rahmatkurniawan] My User - add view model

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/database/FavoriteViewModel.kt
@@ -0,0 +1,32 @@
+package id.co.iconpln.controlflowapp.database
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class FavoriteViewModel(application: Application): AndroidViewModel(application) {
+
+    private  val repository: FavoriteDatabaseRepository
+    private val allFavoriteUsers: LiveData<List<FavoriteUser>>
+
+    init {
+        val favDatabaseDao = FavoriteDatabase.getInstance(application).favoriteDatabaseDao
+        repository = FavoriteDatabaseRepository(favDatabaseDao)
+        allFavoriteUsers = repository.allFavUsers
+    }
+
+    fun getAllFavoriteUsers(): LiveData<List<FavoriteUser>>{
+        return allFavoriteUsers
+    }
+
+    fun insertUser(user: FavoriteUser){
+        // using courotine
+        GlobalScope.launch {
+            repository.insertUser(user)
+            Log.d("@@@Fav", "User ${user.userName} id ${user.userId}  -- INSERTED")
+        }
+    }
+}
