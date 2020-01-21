94f3f58e72de43cd49db4cbf5f147a93379a634c
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-21 13:01:33 +0700

    [rahmatkurniawan] MyProfile  -  add function login in my profile view model

--- app/src/main/java/id/co/iconpln/controlflowapp/myProfileLogin/MyProfileLoginViewModel.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileLogin/MyProfileLoginViewModel.kt
@@ -1,7 +1,15 @@
 package id.co.iconpln.controlflowapp.myProfileLogin
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.co.iconpln.controlflowapp.models.myProfile.ProfileLoginResponse
+import id.co.iconpln.controlflowapp.models.myProfile.ProfileLoginUser
+import id.co.iconpln.controlflowapp.network.MyProfileNetworkRepository
 
 class MyProfileLoginViewModel :ViewModel() {
-
+    fun login(profileLoginUser: ProfileLoginUser)
+    : MutableLiveData<ProfileLoginResponse> {
+        
+        return MyProfileNetworkRepository().doLogin(profileLoginUser)
+    }
 }
