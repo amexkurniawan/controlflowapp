1012952d2fc9a6b187ba05b1d437f2af8aaa6578
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-21 14:08:13 +0700

    [rahmatkurniawan] MyProfile Register - set up for register data

--- app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import id.co.iconpln.controlflowapp.R
+import id.co.iconpln.controlflowapp.models.myProfile.ProfileRegisterUser
 import kotlinx.android.synthetic.main.activity_my_profile_register.*
 
 class MyProfileRegisterActivity : AppCompatActivity(), View.OnClickListener {
@@ -32,8 +34,25 @@ class MyProfileRegisterActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View) {
         when(view.id){
             R.id.btnProfileReg -> {
-                Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+                fetchData(
+                    ProfileRegisterUser(
+                        etProfileRegEmail.text.toString(),
+                        etProfileRegPassword.text.toString(),
+                        etProfileRegName.text.toString(),
+                        etProfileRegHp.text.toString()
+                    )
+                )
             }
         }
+    }
+
+    private fun fetchData(profileRegisterUser: ProfileRegisterUser){
+        viewModel.register(profileRegisterUser).observe(this, Observer { registerResponse ->
+
+            if(registerResponse != null) {
+                Toast.makeText(this, "Success Register ${registerResponse.email}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
