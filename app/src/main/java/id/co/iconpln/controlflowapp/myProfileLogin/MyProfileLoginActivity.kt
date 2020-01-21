80a9f7e5cb4e3eeff41e885b2e79109cfa528fc7
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-21 11:22:52 +0700

    [rahmatkurniawan] MyProfile Login - add my profile login activity and view model
    MyProfile

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileLogin/MyProfileLoginActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.myProfileLogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class MyProfileLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile_login)
+    }
+}
