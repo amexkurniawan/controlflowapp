e9061f596cb8902d3e4e86205d749b4581efc9fc
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-21 11:41:48 +0700

    [rahmatkurniawan] MyProfile Register - add click listener in register form

--- app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileRegister/MyProfileRegisterActivity.kt
@@ -2,12 +2,29 @@ package id.co.iconpln.controlflowapp.myProfileRegister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_my_profile_register.*
 
-class MyProfileRegisterActivity : AppCompatActivity() {
+class MyProfileRegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile_register)
+
+        clickListener()
+    }
+
+    private fun clickListener(){
+        btnProfileReg.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btnProfileReg -> {
+                Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
