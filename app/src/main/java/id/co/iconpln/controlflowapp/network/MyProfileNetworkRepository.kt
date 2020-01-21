26f0fdd66a17702be5c1ae90e3ca0097176bb62b
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-21 12:53:55 +0700

    [rahmatkurniawan] MyProfile Network - addd function login in profle respository

--- app/src/main/java/id/co/iconpln/controlflowapp/network/MyProfileNetworkRepository.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/network/MyProfileNetworkRepository.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import id.co.iconpln.controlflowapp.models.myProfile.BaseProfileResponse
 import id.co.iconpln.controlflowapp.models.myProfile.ProfileLoginResponse
 import id.co.iconpln.controlflowapp.models.myProfile.ProfileLoginUser
+import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfileNetworkRepository {
 
@@ -13,7 +15,32 @@ class MyProfileNetworkRepository {
         val loginData = MutableLiveData<ProfileLoginResponse>()
 
         NetworkConfig.profileApi().loginUser(profileLoginUser)
-            .enqueue(object : Callback<BaseProfileResponse<ProfileLoginResponse>> {})
+            .enqueue(object : Callback<BaseProfileResponse<ProfileLoginResponse>> {
+                override fun onFailure(
+                    call: Call<BaseProfileResponse<ProfileLoginResponse>>,
+                    t: Throwable
+                ) {
+
+                    // on failure code
+                    loginData.postValue(null)
+                }
+
+                override fun onResponse(
+                    call: Call<BaseProfileResponse<ProfileLoginResponse>>,
+                    response: Response<BaseProfileResponse<ProfileLoginResponse>>
+                ) {
+
+                    // on response
+                    if(response.isSuccessful){
+
+                        val loginResponse = response.body()?.data
+                        loginData.postValue(loginResponse)
+
+                    } else {
+                        loginData.postValue(null)
+                    }
+                }
+            })
 
         return loginData
     }
