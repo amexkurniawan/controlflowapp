3af9c35d1371aae64bdc6bd219d5c859b2984619
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-21 11:35:48 +0700

    [rahmatkurniawan] MyProfile Login - add click listener

--- app/src/main/java/id/co/iconpln/controlflowapp/myProfileLogin/MyProfileLoginActivity.kt
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfileLogin/MyProfileLoginActivity.kt
@@ -2,12 +2,33 @@ package id.co.iconpln.controlflowapp.myProfileLogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import id.co.iconpln.controlflowapp.R
+import kotlinx.android.synthetic.main.activity_my_profile_login.*
 
-class MyProfileLoginActivity : AppCompatActivity() {
+class MyProfileLoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile_login)
+
+        clickListener()
+    }
+
+    private fun clickListener() {
+        btnProfileLogin.setOnClickListener(this)
+        tvProfileLoginRegister.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btnProfileLogin -> {
+                Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+            }
+            R.id.tvProfileLoginRegister -> {
+                Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
