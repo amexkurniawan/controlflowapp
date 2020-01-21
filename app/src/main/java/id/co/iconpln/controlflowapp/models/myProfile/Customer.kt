feee582e769f1f928167faf724ed8e35e68d0c96
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-21 12:07:36 +0700

    [rahmatkurniawan] MyProfile - add models for retrofit

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/models/myProfile/Customer.kt
@@ -0,0 +1,15 @@
+package id.co.iconpln.controlflowapp.models.myProfile
+
+data class Customer(
+    val balance: Int,
+    val created_at: String,
+    val device_id: String,
+    val email: String,
+    val id: Int,
+    val is_active: Boolean,
+    val language: String,
+    val name: String,
+    val notif_token: String,
+    val phone: String,
+    val updated_at: String
+)
---
+++ app/src/main/java/id/co/iconpln/controlflowapp/models/myProfile/Data.kt
@@ -0,0 +1,6 @@
+package id.co.iconpln.controlflowapp.models.myProfile
+
+data class Data(
+    val customer: Customer,
+    val token: String
+)
