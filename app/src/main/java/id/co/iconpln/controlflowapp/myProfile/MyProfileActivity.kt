aa90af00c814bb1f03176c5f68921bac6f3b91bf
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-22 11:37:05 +0700

    [rahmatkurniawan] MyProfile  - add MyProfileActivity

---
+++ app/src/main/java/id/co/iconpln/controlflowapp/myProfile/MyProfileActivity.kt
@@ -0,0 +1,13 @@
+package id.co.iconpln.controlflowapp.myProfile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.iconpln.controlflowapp.R
+
+class MyProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+    }
+}
